5e782dfbe46ac91bd3796d7d10a1e5e0a47d8852
Loiane <loianeg@gmail.com>
2019-09-06 14:26:17 -0400

    added error handling

--- web-client/src/app/app.component.ts
+++ web-client/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { VotingService } from './voting.service';
-import { Observable } from 'rxjs';
-import { Candidate } from './models/candidate';
 import { FormControl, Validators } from '@angular/forms';
-import { tap } from 'rxjs/operators';
+import { AlertComponent } from 'ngx-bootstrap/alert/public_api';
+import { Observable } from 'rxjs';
+
+import { Candidate } from './models/candidate';
+import { VotingService } from './voting.service';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +16,9 @@ export class AppComponent implements OnInit {
   list$: Observable<Candidate[]>;
   candidateVote = new FormControl(null, [Validators.required]);
   candidateNew = new FormControl(null, [Validators.required]);
+  savingVote = false;
+  savingCreate = false;
+  alerts: any[] = [];
 
   constructor(private service: VotingService) {}
 
@@ -27,20 +31,50 @@ export class AppComponent implements OnInit {
   }
 
   castVote() {
-    console.log(this.candidateVote.value);
+    this.savingVote = true;
     this.service.castVote(this.candidateVote.value)
     .subscribe(res => {
-      console.log(res);
+      this.savingVote = false;
       this.listAllCandidates();
+      this.alerts.push({
+        type: 'success',
+        msg: 'Vote casted successfully for candidate ' + this.candidateVote.value,
+        timeout: 5000
+      });
+    },
+    error => {
+      this.savingVote = false;
+      this.alerts.push({
+        type: 'danger',
+        msg: 'Candidate does not exist.',
+        timeout: 5000
+      });
     });
   }
 
   createCandidate() {
-    console.log(this.candidateNew.value);
+    this.savingCreate = true;
     this.service.create(this.candidateNew.value)
     .subscribe(res => {
-      console.log(res);
+      this.savingCreate = false;
       this.listAllCandidates();
+      this.alerts.push({
+        type: 'success',
+        msg: 'Candidate ' + this.candidateNew.value + ' created successfully.',
+        timeout: 5000
+      });
+    },
+    error => {
+      this.savingCreate = false;
+      this.alerts.push({
+        type: 'danger',
+        msg: 'Candidate already exists.',
+        timeout: 5000
+      });
     });
+  }
+
+  onClosed(dismissedAlert: AlertComponent) {
+    this.alerts = this.alerts.filter(alert => alert !== dismissedAlert);
   }
 }
--- web-client/src/app/app.module.ts
+++ web-client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AlertModule } from 'ngx-bootstrap/alert';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
